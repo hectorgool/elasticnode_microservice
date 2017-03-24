54a75146396516fa1f1a78e927037b5ba7c170bc
hectorgool <hectorgool@gmail.com>
2017-03-23 20:20:48 -0600

    lint format

--- README.md
+++ README.md
@@ -56,7 +56,7 @@ sh elasticsearch_index_mappings.sh
 
 ### 9. Load documents:
 
-```sh documents.sh
+```sh
 sh documents.sh
 ```
 
@@ -113,4 +113,4 @@ curl -i -H "Accept: application/json" -H "Content-Type: application/json" http:/
 * http://expressjs.com/es/api.html
 * https://www.cyberciti.biz/faq/linux-unix-vim-find-replace-text-string-words/
 * https://aws.amazon.com/es/elasticsearch-service/faqs/
-
+* https://www.npmjs.com/package/standard
--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-'use strict';
+'use strict'
 
-var express    = require('express');
-var app        = express();
-var es         = require('./elasticsearch')();
-var bodyParser = require('body-parser');
-var config     = require('./config');
+var express = require('express')
+var app = express()
+var es = require('./elasticsearch')()
+var bodyParser = require('body-parser')
+var config = require('./config')
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-//curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:3000
-app.get('/', function(req, res, next) {
-	res.status(200).json({ hello: 'world'});
-});
+// curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:3000
+app.get('/', function (req, res, next) {
+  res.status(200).json({ hello: 'world' })
+})
 
-//curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:3000/ping
-app.get('/ping', function(req, res, next) {
-  es.ping(function(data){
-    res.status(200).json( { data: data } );
-  });
-});
+// curl -i -H "Accept: application/json" -H "Content-Type: application/json" http://localhost:3000/ping
+app.get('/ping', function (req, res, next) {
+  es.ping(function (data) {
+    res.status(200).json({ data: data })
+  })
+})
 
 // curl -d '{"term":"villa"}' -H "Content-Type: application/json" http://127.0.0.1:3000/search
-app.post('/search', function(req, res, next) {
-  es.searchDocument(req.body.term, function(data){
-    res.status(200).json( { data: data } );
-  });
-});
+app.post('/search', function (req, res, next) {
+  es.searchDocument(req.body.term, function (data) {
+    res.status(200).json({ data: data })
+  })
+})
 
-app.get('/exists_document/:id', function(req, res, next) {
-  es.existsDocument( req.params.id, function(data){
-    res.status(200).json( { data: data } );
-  });
-});
+app.get('/exists_document/:id', function (req, res, next) {
+  es.existsDocument(req.params.id, function (data) {
+    res.status(200).json({ data: data })
+  })
+})
 
-app.post('/update_document', function(req, res, next) {
-  es.updateDocument(req.body, function(data){
-    res.status(200).json( { data: data } );
-  });
-});
+app.post('/update_document', function (req, res, next) {
+  es.updateDocument(req.body, function (data) {
+    res.status(200).json({ data: data })
+  })
+})
 
 /*
 app.get('/search/:id', function(req, res, next) {
@@ -48,5 +48,5 @@ app.get('/search/:id', function(req, res, next) {
 });
 */
 
-app.listen(config.server.port);
-console.log('Express started on port 3000');
+app.listen(config.server.port)
+console.log('Express started on port 3000')
